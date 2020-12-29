f6abd015dddec7d02e4cff730283a52700875022
petrero <petrero@gmail.com>
2020-12-29 18:37:29 +0200

    2.3. Building the Inbox
    Give the ability to the user to mark emails as read

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
           v-for="email in emails"
           :key="email.id"
           :class="['clickable', email.read ? 'read' : '']"
+          @click="email.read = true"
       >
         <td><input type="checkbox"></td>
         <td>{{ email.from }}</td>
