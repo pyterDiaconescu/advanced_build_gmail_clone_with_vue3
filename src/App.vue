d3a01d17065b45e1eb6137f029d31c39dc55447a
petrero <petrero@gmail.com>
2020-12-29 18:46:55 +0200

    2.4. Building the Inbox
    Format dates to look better

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,20 @@
         <td>
           <p><strong>{{ email.subject}}</strong> - {{ email.body }}</p>
         </td>
-        <td>{{ email.sentAt }}</td>
+        <td>{{ format(new Date(email.sentAt), 'MMM do yyyy') }}</td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import { format } from 'date-fns'
 
 export default {
   name: 'App',
   data(){
     return {
+      format,
       "emails": [
         {
           "id": 1,
