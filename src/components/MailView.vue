aae8e738c93b218e53056999d882e57658e5ba28
petrero <petrero@gmail.com>
2020-12-31 09:00:13 +0200

    9.1. Email Action Bar
    Button that toggles read state of the email

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="email-display">
+    <div>
+      <button>Archive</button>
+      <button @click="toggleRead">{{ email.read ? 'Mark Unread' : 'Mark Read' }}</button>
+      <button>Newer</button>
+      <button>Older</button>
+    </div>
     <h2 class="mb-0">Subject: <strong>{{ email.subject }}</strong></h2>
     <div><em>From {{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyyy') }}</em></div>
     <div v-html="marked(email.body)" />
@@ -9,12 +15,19 @@
 <script>
 import { format } from 'date-fns'
 import marked from 'marked'
+import axios from "axios"
 
 export default {
-  setup(){
+  setup(props){
+    let email = props.email
+    let toggleRead = () => {
+      email.read = !email.read
+      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    }
     return {
       format,
-      marked
+      marked,
+      toggleRead
     }
   },
   props: {
