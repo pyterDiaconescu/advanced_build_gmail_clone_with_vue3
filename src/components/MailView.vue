54d876e3bb35f5f38d4e8d1ded7c6292cc2ceac7
petrero <petrero@gmail.com>
2020-12-31 09:13:38 +0200

    9.2. Email Action Bar
    Button that toggles archived state of the email and create key shortcuts for button actions

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="email-display">
     <div>
-      <button>Archive</button>
-      <button @click="toggleRead">{{ email.read ? 'Mark Unread' : 'Mark Read' }}</button>
-      <button>Newer</button>
-      <button>Older</button>
+      <button @click="toggleArchive">{{ email.archived ? 'Move to Inbox (e)' : 'Archive (e)' }}</button>
+      <button @click="toggleRead">{{ email.read ? 'Mark Unread (r)' : 'Mark Read (r)' }}</button>
+      <button @click="goNewer">Newer (k)</button>
+      <button @click="goOlder">Older (j)</button>
     </div>
     <h2 class="mb-0">Subject: <strong>{{ email.subject }}</strong></h2>
     <div><em>From {{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyyy') }}</em></div>
@@ -15,6 +15,7 @@
 <script>
 import { format } from 'date-fns'
 import marked from 'marked'
+import useKeydown from '@/composables/use-keydown'
 import axios from "axios"
 
 export default {
@@ -24,10 +25,18 @@ export default {
       email.read = !email.read
       axios.put(`http://localhost:3000/emails/${email.id}`, email)
     }
+    let toggleArchive = () => {
+      email.archived = !email.archived
+      axios.put(`http://localhost:3000/emails/${email.id}`, email)
+    }
+    useKeydown([
+      {key: 'r', fn: toggleRead}
+    ])
     return {
       format,
       marked,
-      toggleRead
+      toggleRead,
+      toggleArchive
     }
   },
   props: {
